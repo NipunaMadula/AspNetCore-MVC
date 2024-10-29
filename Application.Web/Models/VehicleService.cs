b850d6357ddf189bdd6248a890862b4a83bd1e44
Nipuna Madula <nipunamadula2001@gmail.com>
2024-10-30 00:23:20 +0530

    Add properties to VehicleService model

--- Application.Web/Models/VehicleService.cs
+++ Application.Web/Models/VehicleService.cs
@@ -2,5 +2,10 @@
 {
     public class VehicleService
     {
+        public int ServiceId { get; set; }
+        public string ServiceCategory { get; set; }
+        public string Details { get; set; }
+        public string Price { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
