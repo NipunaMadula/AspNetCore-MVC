1134dac7c046dda5ec11d32595c4882ae3c3439b
Nipuna Madula <nipunamadula2001@gmail.com>
2024-10-30 00:46:17 +0530

    Add Overview action with sample VehicleService data

--- Application.Web/Controllers/ServicesController.cs
+++ Application.Web/Controllers/ServicesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Web.Controllers
 {
@@ -6,6 +7,17 @@ namespace Application.Web.Controllers
     {
         public IActionResult Index()
         {
+            return View();
+        }
+        public IActionResult Overview()
+        {
+            var service = new VehicleService();
+            service.ServiceId = 1;
+            service.ServiceCategory = "Gas Fill";
+            service.Details = "Refueling service with premium gasoline";
+            service.Price = "50.00";
+            service.IsDeleted = false;
+
             return View();
         }
     }
